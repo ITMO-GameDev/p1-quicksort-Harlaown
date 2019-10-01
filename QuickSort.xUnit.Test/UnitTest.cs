94993b52c637fd026bc504a800b9c6527290838a
Mikhail <raeinforce@gmail.com>
2019-10-01 12:39:33 +0300

    add UnitTest project
    Unit tests run with Net core 3.0 and xUnit

---
+++ QuickSort.xUnit.Test/UnitTest.cs
@@ -0,0 +1,331 @@
+using QuickSortProject;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace QuickSort.xUnit.Test
+{
+    public class UnitTest
+    {
+        private const int SmallSize = 10;
+        private const int MediumSize = 150;
+        private const int BigSize = 1500;
+        private const int VeryBigSize = sizeof(Int32) << 8 << 10;
+
+        private static readonly Random rnd = new Random((int)DateTime.Now.Ticks);
+
+        private static void RandomArray(int[] array)
+        {
+            Parallel.For(0, array.Length, index => { array[index] = rnd.Next(); });
+        }
+
+        private static void RandomArray<T>(T[] array, Func<T> func)
+        {
+            Parallel.For(0, array.Length, index => { array[index] = func(); });
+        }
+
+        private static void Shuffle<T>(ref Random rng, T[] array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+
+        private static bool IsSorted(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i - 1] > arr[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool IsSorted<T>(T[] arr, IComparer<T> comparer)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (comparer.Compare(arr[i - 1], arr[i]) > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static readonly Comparer<int> Comparer = Comparer<int>.Create((i, i1) => i.CompareTo(i1));
+
+        [Fact]
+        public void SmallArraySort()
+        {
+            int[] SmallArray = new int[SmallSize];
+            RandomArray(SmallArray);
+
+            SmallArray.HybridSort(Comparer<int>.Default);
+
+            var condition = IsSorted(SmallArray);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void MediumArraySort()
+        {
+            int[] MediumArray = new int[MediumSize];
+            RandomArray(MediumArray);
+
+            MediumArray.HybridSort(Comparer);
+
+            var condition = IsSorted(MediumArray);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void BigArraySort()
+        {
+            int[] BigArray = new int[BigSize];
+            RandomArray(BigArray);
+
+            BigArray.HybridSort(Comparer);
+
+            var condition = IsSorted(BigArray);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void VeryBigArraySort()
+        {
+            int[] VeryBigArray = new int[VeryBigSize];
+            RandomArray(VeryBigArray);
+
+            VeryBigArray.HybridSort(Comparer);
+
+            var condition = IsSorted(VeryBigArray);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void SmallSortedSortArray()
+        {
+            int[] SmallArray = new int[SmallSize];
+            RandomArray(SmallArray);
+            Array.Sort(SmallArray);
+
+            SmallArray.HybridSort(Comparer);
+
+            var condition = IsSorted(SmallArray);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void MediumSortedSortArray()
+        {
+            int[] array = new int[MediumSize];
+            RandomArray(array);
+            Array.Sort(array);
+
+            array.HybridSort(Comparer);
+
+            var condition = IsSorted(array);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void BigSortedSortArray()
+        {
+            int[] array = new int[BigSize];
+            RandomArray(array);
+            Array.Sort(array);
+
+            array.HybridSort(Comparer);
+
+            var condition = IsSorted(array);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void VeryBigSortedSortArray()
+        {
+            int[] array = new int[VeryBigSize];
+            RandomArray(array);
+            Array.Sort(array);
+
+            array.HybridSort(Comparer);
+
+            var condition = IsSorted(array);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void SmallCustomArraySort()
+        {
+            Vector2[] array = new Vector2[SmallSize];
+            RandomArray(array, () =>
+            {
+                Vector2 temp = new Vector2(rnd.Next(), rnd.Next());
+                return temp;
+            });
+            var comparer = Comparer<Vector2>.Create(((self, other) =>
+            {
+                if (self.X > other.X) return 1;
+                if (self.X < other.X) return -1;
+                if (self.Y > other.Y) return 1;
+                if (self.Y < other.Y) return -1;
+                return 0;
+            }));
+
+            array.HybridSort(comparer);
+
+            var condition = IsSorted(array, comparer);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void MediumCustomArraySort()
+        {
+            Vector2[] array = new Vector2[MediumSize];
+            RandomArray(array, () =>
+            {
+                Vector2 temp = new Vector2(rnd.Next(), rnd.Next());
+                return temp;
+            });
+            var comparer = Comparer<Vector2>.Create(((self, other) =>
+            {
+                if (self.X > other.X) return 1;
+                if (self.X < other.X) return -1;
+                if (self.Y > other.Y) return 1;
+                if (self.Y < other.Y) return -1;
+                return 0;
+            }));
+
+            array.HybridSort(comparer);
+
+            var condition = IsSorted(array, comparer);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void BigCustomArraySort()
+        {
+            Vector2[] array = new Vector2[BigSize];
+            RandomArray(array, () =>
+            {
+                Vector2 temp = new Vector2(rnd.Next(), rnd.Next());
+                return temp;
+            });
+            var comparer = Comparer<Vector2>.Create(((self, other) =>
+            {
+                if (self.X > other.X) return 1;
+                if (self.X < other.X) return -1;
+                if (self.Y > other.Y) return 1;
+                if (self.Y < other.Y) return -1;
+                return 0;
+            }));
+
+            array.HybridSort(comparer);
+
+            var condition = IsSorted(array, comparer);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void VeryBigCustomArraySort()
+        {
+            Vector2[] array = new Vector2[VeryBigSize];
+            RandomArray(array, () =>
+            {
+                Vector2 temp = new Vector2(rnd.Next(), rnd.Next());
+                return temp;
+            });
+            var comparer = Comparer<Vector2>.Create(((self, other) =>
+            {
+                if (self.X > other.X) return 1;
+                if (self.X < other.X) return -1;
+                if (self.Y > other.Y) return 1;
+                if (self.Y < other.Y) return -1;
+                return 0;
+            }));
+
+            array.HybridSort(comparer);
+
+            var condition = IsSorted(array, comparer);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void SmallReverseArraySort()
+        {
+            var array = new int[SmallSize];
+            RandomArray(array);
+            Array.Sort(array);
+            Array.Reverse(array);
+
+            array.HybridSort(Comparer);
+            var condition = IsSorted(array);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void MediumReverseArraySort()
+        {
+            var array = new int[MediumSize];
+            RandomArray(array);
+            Array.Sort(array);
+            Array.Reverse(array);
+
+            array.HybridSort(Comparer);
+            var condition = IsSorted(array);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void BigReverseArraySort()
+        {
+            var array = new int[BigSize];
+            RandomArray(array);
+            Array.Sort(array);
+            Array.Reverse(array);
+
+            array.HybridSort(Comparer);
+            var condition = IsSorted(array);
+
+            Assert.True(condition);
+        }
+
+        [Fact]
+        public void VeryBigReverseArraySort()
+        {
+            var array = new int[VeryBigSize];
+            RandomArray(array);
+            Array.Sort(array);
+            Array.Reverse(array);
+
+            array.HybridSort(Comparer);
+            var condition = IsSorted(array);
+
+            Assert.True(condition);
+        }
+    }
+}
